ea688669653426fc2a407f6b08eb3bc88ceb3d48
alekseiKromski <aleksei.kromski@outlook.com>
2023-03-11 11:10:21 +0200

    front-end: init react app

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Client still in progress</h1>
+        <img src={require("./images/dogo.gif")} alt=""/>
+        <div className="">
+            <a href="https://github.com/AlekseiKromski/alekseikromski-blog" target="_blank">Github project</a>
+        </div>
+    </div>
+  );
+}
+
+export default App;
