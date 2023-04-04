d3e048b2f56df9629b5463887f706df3695ddd58
alekseiKromski <aleksei.kromski@outlook.com>
2023-04-04 23:01:10 +0300

    dbstore: migration feature implementation started
    Currently, the application will try to find (just return all models for now) models (tables) that we need to create by migration (Check TODOs)

--- api/storage/dbstore/dbstore.go
+++ api/storage/dbstore/dbstore.go
@@ -5,9 +5,9 @@ import (
 	"alekseikromski.com/blog/api/storage/models"
 	"database/sql"
 	"fmt"
-	"log"
-
 	_ "github.com/lib/pq"
+	"log"
+	"reflect"
 )
 
 type DbConnection struct {
@@ -20,16 +20,24 @@ func NewDbConnection(username, password, hostname, port, database string) (*DbCo
 	if err != nil {
 		return nil, fmt.Errorf("cannot create database connection: %w", err)
 	}
-	log.Println("[INFO] Pinging database")
+	log.Println("[DBSTORAGE] Pinging database")
 	err = db.Ping()
 	if err != nil {
 		return nil, fmt.Errorf("cannot ping database: %w", err)
 	}
 
-	log.Println("[INFO] Connected to database")
-	return &DbConnection{
+	log.Println("[DBSTORAGE] Connected to database")
+
+	dbConnection := &DbConnection{
 		Connection: db,
-	}, nil
+	}
+
+	log.Println("[DBSTORAGE] Run migrations")
+	if err = dbConnection.RunMigrations(); err != nil {
+		return nil, fmt.Errorf("trying to run all migrations: %v", err)
+	}
+
+	return dbConnection, err
 }
 
 func (db *DbConnection) GetPosts(request *storage.QueryRequest) []*models.Post {
@@ -42,4 +50,42 @@ func (db *DbConnection) CreatePost(post *models.Post) (bool, error) {
 
 func (db *DbConnection) Stop() {
 	db.Connection.Close()
+}
+
+func (db *DbConnection) RunMigrations() error {
+	ms := db.getExistedTables()
+
+	//TODO: make as transaction
+	for _, model := range ms {
+		if m, ok := model.(models.Migration); ok {
+			sql := m.TableCreate()
+
+			_, err := db.Connection.Exec(sql)
+			if err != nil {
+				return fmt.Errorf("cannot make migration: %s, %v", sql, err)
+			}
+
+			log.Printf("[DBSTORAGE] migrations for: %s SUCCESSFUL", db.getType(m))
+		}
+	}
+
+	return nil
+}
+
+func (db *DbConnection) getType(strct interface{}) string {
+	if t := reflect.TypeOf(strct); t.Kind() == reflect.Ptr {
+		return t.Elem().Name()
+	} else {
+		return t.Name()
+	}
+}
+
+func (db *DbConnection) getExistedTables() []interface{} {
+	//TODO create a query and get all migrations
+	return []interface{}{
+		&models.MigrationModel{},
+		&models.Tag{},
+		&models.Post{},
+		&models.Category{},
+	}
 }
