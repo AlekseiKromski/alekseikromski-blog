ca3a15dae3a7b67248e94fbe1bcd914991ab7a4b
alekseiKromski <aleksei.kromski@outlook.com>
2023-04-25 18:47:19 +0300

    router: fix problem with long and short routes

--- router/route.go
+++ router/route.go
@@ -42,10 +42,14 @@ func (ro *Route) Match(r []string, method string) (bool, Params) {
 }
 
 func (ro *Route) match(r []string) bool {
+	if len(r) != len(ro.PathList) {
+		return false
+	}
 	for index, item := range ro.PathList {
 		if item.Key == "*" {
 			break
 		}
+
 		if !item.RegExp.Match([]byte(r[index])) {
 			return false
 		}
