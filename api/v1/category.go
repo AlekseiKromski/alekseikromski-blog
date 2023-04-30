76bef9be5c23ac3d34fa1e1bd76ad8c2af6217ab
alekseiKromski <aleksei.kromski@outlook.com>
2023-04-30 16:41:52 +0300

    v1: added category creation ability

--- api/v1/category.go
+++ api/v1/category.go
@@ -92,3 +92,39 @@ func (v *V1) UpdateCategory(w http.ResponseWriter, r *http.Request) {
 
 	v.ReturnResponse(w, []byte("OK"))
 }
+
+// CreateCategory
+//
+//	@Summary		Create category
+//	@Description	Create a category
+//	@Success		200
+//	@Failure		400
+//	@Failure		500
+//	@Router			/V1/category/create [post]
+func (v *V1) CreateCategory(w http.ResponseWriter, r *http.Request) {
+	var category models.Category
+
+	err := json.NewDecoder(r.Body).Decode(&category)
+	defer r.Body.Close()
+	if err != nil {
+		log.Printf("Decoding error: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	created, err := v.storage.CreateCategory(&category)
+	if err != nil {
+		log.Printf("Problem: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if !created {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("created"))
+	log.Printf("Category with id [%d] was created", category.ID)
+}
