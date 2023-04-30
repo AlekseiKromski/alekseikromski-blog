a5561e278bc7109db5f9057b717150c090bf2eb3
alekseiKromski <aleksei.kromski@outlook.com>
2023-04-30 18:46:59 +0300

    v1: added search mechanism

---
+++ api/storage/dbstore/search.go
@@ -0,0 +1,64 @@
+package dbstore
+
+import (
+	"alekseikromski.com/blog/api/storage"
+	"alekseikromski.com/blog/api/storage/models"
+	"log"
+)
+
+func (db *DbConnection) Search(sr *storage.SearchRequest) *storage.SearchResult {
+	result := &storage.SearchResult{
+		Posts: []*models.Post{},
+	}
+
+	result.Posts = getPostSearch(sr, db)
+
+	return result
+}
+
+func getPostSearch(sr *storage.SearchRequest, db *DbConnection) []*models.Post {
+	posts := []*models.Post{}
+
+	query := models.SearchPost(sr.Search)
+	rows, err := db.Connection.Query(query)
+	if err != nil {
+		log.Printf("Problem: %v", err)
+		return posts
+	}
+
+	for rows.Next() {
+		post := models.CreatePost()
+		var scanError error
+		category := models.CreateCategory()
+		scanError = rows.Scan(
+			&post.ID,
+			&post.Title,
+			&post.Description,
+			&post.CategoryID,
+			&post.CreatedAt,
+			&post.UpdatedAt,
+			&post.DeletedAt,
+			&category.ID,
+			&category.Name,
+			&category.CreatedAt,
+			&category.UpdatedAt,
+			&category.DeletedAt,
+		)
+
+		post.Category = category
+
+		if scanError != nil {
+			log.Printf("troubles during scanning: %w", err)
+		}
+
+		//Get comments
+		post.Comments = db.GetComments(post.ID)
+
+		//Get tags
+		post.Tags = db.GetTags(&post.ID)
+
+		posts = append(posts, post)
+	}
+
+	return posts
+}
