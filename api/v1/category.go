91e8fb3bf7a678ec499648ae98e9267f870b5fae
alekseiKromski <aleksei.kromski@outlook.com>
2023-04-30 16:23:25 +0300

    v1: added category update ability

--- api/v1/category.go
+++ api/v1/category.go
@@ -1,8 +1,10 @@
 package v1
 
 import (
+	"alekseikromski.com/blog/api/storage/models"
 	"alekseikromski.com/blog/router"
 	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -57,6 +59,35 @@ func (v *V1) DeleteCategory(w http.ResponseWriter, r *http.Request) {
 
 	if err := v.storage.DeleteCategory(id); err != nil {
 		v.ReturnErrorResponse(NewInputError(), w)
+	}
+
+	v.ReturnResponse(w, []byte("OK"))
+}
+
+// UpdateCategory
+//
+//	@Summary		Update category
+//	@Description	Update single category
+//	@Produce		json
+//	@Success		200	{array}		models.Post
+//	@Failure		400	{object}	V1.JsonError	"if we cannot decode or encode payload"
+//	@Failure		500	{object}	V1.InputError	"if we have bad payload"
+//	@Router			/v1/category/edit [post]
+func (v *V1) UpdateCategory(w http.ResponseWriter, r *http.Request) {
+	categoryForUpdate := models.CreateCategory()
+
+	err := json.NewDecoder(r.Body).Decode(&categoryForUpdate)
+	defer r.Body.Close()
+	if err != nil {
+		log.Printf("Decoding error: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err = v.storage.UpdateCategory(categoryForUpdate); err != nil {
+		log.Printf("Update error: %s", err.Error())
+		v.ReturnErrorResponse(NewDecodingError(), w)
+		return
 	}
 
 	v.ReturnResponse(w, []byte("OK"))
